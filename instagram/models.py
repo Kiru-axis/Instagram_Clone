a277b1ec5aa4a2c1d6b791140e208d65b2946b56
Kiru-axis <peter.kiru@student.moringaschool.com>
2021-07-12 01:10:01 +0300

    posts class

--- instagram/models.py
+++ instagram/models.py
@@ -11,3 +11,12 @@ class Profile(models.Model):
     name = models.CharField(blank=True, max_length=120)
     location = models.CharField(max_length=60, blank=True)
 
+
+# post model class
+class Post(models.Model):
+    image = models.ImageField(upload_to='posts/')
+    name = models.CharField(max_length=250, blank=True)
+    caption = models.CharField(max_length=250, blank=True)
+    likes = models.ManyToManyField(User, related_name='likes', blank=True, )
+    user = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='posts')
+    created = models.DateTimeField(auto_now_add=True, null=True)
