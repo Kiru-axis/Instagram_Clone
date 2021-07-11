49e8ec4fac0ad96f5fe5747ce1bc29f80927e515
Kiru-axis <peter.kiru@student.moringaschool.com>
2021-07-12 01:05:27 +0300

    signup form template and route

--- instagram/urls.py
+++ instagram/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from . import views
+
 
 urlpatterns = [
+    path('signup/', views.signup, name='signup'),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
